e029fc33b6663c9ed04287bcbb74100d67149339
AhmedGarcia <166225449+AhmedGarcia@users.noreply.github.com>
2024-09-22 15:57:21 -0400

    created Book.js file under models. populated it

---
+++ server/models/Book.js
@@ -0,0 +1,36 @@
+//Import mongoose
+const { Schema, model } = require('mongoose');
+
+const bookSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    author: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    description: {
+        type: String,
+        trim: true,
+    },
+    image : {
+        type: String, //URL to the book cover image
+        trim: true,
+        required:false
+    },
+    reviews: [{
+        type: Schema.Types.Objectid,
+        ref: 'Review',
+    }],
+    }, {
+        toJSON: {
+            virtuals: true,
+        },
+});
+
+const Book = model('Book', bookSchema);
+
+module.exports = Book;
