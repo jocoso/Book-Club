3cbdc40c9705f0bb3c9530de109ebab9bf19e66d
AhmedGarcia <166225449+AhmedGarcia@users.noreply.github.com>
2024-09-22 19:28:37 -0400

    created Post.js in models,and populated it

---
+++ server/models/Post.js
@@ -0,0 +1,43 @@
+const { Schema, model } = require('mongoose');
+
+const postSchema = new Schema(
+    {
+        title: {
+            typr: String,
+            required: true,
+        },
+        content: {
+            type: String,
+            required: true,
+        },
+        author: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+        },
+        blob: {
+            type: Number,
+            default: 0,
+        },
+        media: [
+            {
+                type: String,
+            },
+        ],
+        comments: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Comment',
+            }
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+    }
+);
+
+const Post = model('Post', postSchema);
+
+module.exports = Post; 
