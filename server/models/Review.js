14606d8bb803882278d7c54825c035655f5ac502
AhmedGarcia <166225449+AhmedGarcia@users.noreply.github.com>
2024-09-22 16:12:01 -0400

    created Review.js under models and populated it

---
+++ server/models/Review.js
@@ -0,0 +1,38 @@
+// Import Mongoose
+const { Schema, model } = require('mongoose');
+
+// Define the Review Schema
+const reviewSchema = new Schema({
+    reviewText: {
+        type: String,
+        required: 'You need to leave a review!',
+        minlength: 1,
+        maxlength: 280,
+    },
+    rating: {
+        type: Number,
+        required: true,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    bookId: {
+        type: Schema.Types.objectId,
+        ref: 'Book',
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+}, {
+    toJSON: {
+        virtuals: true,
+    }
+});
+
+//Export Review model
+const Review = model('Review', reviewSchema);
+
+module.exports = Review;
