2d4b466ff41813b1b3f849f0643bdeee94021d94
jocoso <joshua.collado022@gmail.com>
2024-09-25 22:52:08 -0400

    Added librarian class to fetch Google Books book data

---
+++ server/utils/librarian.js
@@ -0,0 +1,17 @@
+class Librarian {
+    constructor(apiUrl) {
+        this.apiUrl = apiUrl;
+    }
+
+    // Private
+    async fetcher(endPoint) {
+        // Fetching logic goes here
+    }
+
+    // Public
+    async getBook(bookId) {
+        // Logic goes here
+    }
+}
+
+module.exports = Librarian;
