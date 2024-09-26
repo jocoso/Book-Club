83c266009a4e2e048faeeb3f86fe5db3209756d4
jocoso <joshua.collado022@gmail.com>
2024-09-26 19:16:22 -0400

    added comments to librarian.js

--- server/utils/librarian.js
+++ server/utils/librarian.js
@@ -4,13 +4,19 @@ class Librarian {
     }
 
     // Private
+    validate(endPoint) {
+        // return true if endPoint is valid
+        return false;
+    }
     async fetcher(endPoint) {
         // Fetching logic goes here
+        // Return object if successful
+        // Return null if not
     }
 
     // Public
     async getBook(bookId) {
-        // Logic goes here
+        // Logic to manage everything else goes here
     }
 }
 
