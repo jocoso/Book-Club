ae03124a63f4c084f800926af7bd549d7a289250
jocoso <joshua.collado022@gmail.com>
2024-10-01 05:12:30 -0400

    modified package.json

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,23 +1,27 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import path from "path";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path';
+import dotenv from 'dotenv';
+
+// Load environment variables from .env file
+dotenv.config();
 
 export default defineConfig({
-    plugins: [vue()],
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-        },
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
-    server: {
-        host: "0.0.0.0",
-        port: process.env.PORT || 3000, // Port fallback
-        proxy: {
-            "/graphql": {
-                target: import.meta.env.VITE_API_URL || "http://localhost:3001",
-                changeOrigin: true,
-                secure: false,
-            },
-        },
+  },
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 3000, // Port fallback
+    proxy: {
+      '/graphql': {
+        target: process.env.VITE_API_URL || 'http://localhost:3001', // Use process.env here
+        changeOrigin: true,
+        secure: false,
+      },
     },
+  },
 });
