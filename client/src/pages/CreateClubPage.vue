0429babeaa745f6f98b8314acedae7464e3b71c7
sickcurse <111002422+sickcurse@users.noreply.github.com>
2024-10-01 16:27:16 -0400

    added new page

---
+++ client/src/pages/CreateClubPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1>Create a New Club</h1>
+    <CreateClubForm />
+  </div>
+</template>
+
+<script>
+import CreateClubForm from '../components/CreateClubForm.vue';
+
+export default {
+  components: {
+    CreateClubForm
+  }
+};
+</script>
+
+<style scoped>
+/* Add your styles here */
+</style>
