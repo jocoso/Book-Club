116d41291134a36a37b71396de61c0f9108e81eb
jocoso <joshua.collado022@gmail.com>
2024-10-01 04:24:50 -0400

    modified package.json

--- server/server.js
+++ server/server.js
@@ -58,7 +58,7 @@ const configureExpress = (app) => {
     if (process.env.NODE_ENV === "production") {
         app.use(express.static(path.join(__dirname, "../client/build")));
         app.get("*", (req, res) => {
-            res.sendFile(path.join(__dirname, "../client/build/index.html"));
+            res.sendFile(path.join(__dirname, "../client/dist/index.html"));
         });
     }
 };
