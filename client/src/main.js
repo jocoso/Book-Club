60f76c0066b6e16e0972389c151cd039b6f6a906
jocoso <joshua.collado022@gmail.com>
2024-10-01 19:49:44 -0400

    Adding a more detailed cors origin

--- client/src/main.js
+++ client/src/main.js
@@ -1,15 +1,8 @@
 import { createApp, provide, h } from "vue";
 import { DefaultApolloClient } from "@vue/apollo-composable";
-import { ApolloClient, InMemoryCache } from "@apollo/client/core";
+import apolloClient from './apolloClient';
 import App from "./App.vue";
 import router from "./router";
-
-// Use Vite's environment variable for API URL
-const cache = new InMemoryCache();
-const apolloClient = new ApolloClient({
-    uri: `${import.meta.env.VITE_API_URL}/graphql` || "http://localhost:3001/graphql", // Use environment variable here
-    cache,
-});
 
 createApp({
     setup() {
