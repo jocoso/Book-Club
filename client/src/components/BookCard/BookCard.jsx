8e8c188331d9e052f1564ade7551f9b8f6d8faf1
jocoso <joshua.collado022@gmail.com>
2024-10-02 15:26:56 -0400

    Added an adaptable grid

--- client/src/components/BookCard/BookCard.jsx
+++ client/src/components/BookCard/BookCard.jsx
@@ -1,10 +1,12 @@
+import Title from '../Title'
+
 const BookCard = ({ book }) => {
     return (
-        <div>
-            <h3>{book.title}</h3>
+        <div className="w-60">
+            <Title tier={4} className='!text-left'>{book.title}</Title>
             <p>{book.author}</p>
-            <p>{book.description}</p>
-            {book.image && <img src={book.image} alt={book.title} />}
+            <p className="truncate">{book.description}</p>
+            {book.image && <img src={book.image} alt={book.title} className="mx-auto" />}
         </div>
     );
 };
