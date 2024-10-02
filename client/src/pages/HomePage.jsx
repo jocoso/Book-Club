8cd286e8a604b0e18b521e65930815f54b7a012f
jocoso <joshua.collado022@gmail.com>
2024-10-02 15:47:43 -0400

    Styled communities

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -4,16 +4,24 @@ import { LimitedBookList } from "../components/BookList";
 
 const Homepage = () => {
     return (
-        <div className="flex flex-col w-8/12 mx-auto h-full">
+        <div className="flex flex-col w-full max-w-7xl mx-auto px-4 py-10 space-y-10">
             {/* Main Title */}
-            <Title>Welcome to the Book Club Hub</Title>
-            <p className="text-center mt-10">Your ultimate platform for book clubs!</p>
+            <Title className="text-center text-4xl font-bold">Welcome to the Book Club Hub</Title>
+            <p className="text-center text-lg text-gray-700">
+                Your ultimate platform for book clubs!
+            </p>
 
             {/* Link to the user list */}
-            <StyledLink to="/users" className="text-center mb-10">View All Users</StyledLink>
+            <div className="text-center">
+                <StyledLink to="/users" className="text-indigo-500 text-lg font-semibold hover:text-indigo-700">
+                    View All Users
+                </StyledLink>
+            </div>
 
-            {/* Section Title */}
-            <Title tier={3} className="mb-10">Latest Books</Title>
+            {/* Section Title for Latest Books */}
+            <Title tier={3} className="text-center text-2xl font-semibold mb-6">
+                Latest Books
+            </Title>
 
             {/* Book List */}
             <LimitedBookList />
